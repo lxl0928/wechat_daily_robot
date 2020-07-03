f07a0c1dd1259747bd4c47a57e9d1bd03bfad77b
lixiaolong <lixiaolong@sensoro.com>
2020-07-03 18:44:00 +0800

    fix: 周五发送其它周报更新的通知

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import time
+import logging
 
 from timi_robot import SensoroLoggerClient
 
@@ -11,17 +12,17 @@ sensoro_logger = SensoroLoggerClient(url=ROBOT_URL, phones=ACCEPT_PHONES)
 
 class SensoroDailyNotice(object):
 
-    def judge_time(self):
+    def run(self):
         while True:
             localtime = time.localtime(time.time() + 8 * 60 * 60)
             tm_wday = localtime.tm_wday + 1
             tm_hour = localtime.tm_hour
-            print(tm_wday, tm_hour)
             if tm_wday in [1, 2, 3, 4, 5]:  # 周一 ~ 周五
                 if tm_hour in (10, 19):  # 早10，晚19
+                    logging.info("当前时间{}, {}, {}".format(localtime, tm_wday, tm_hour))
                     self.send_notice(wday=tm_wday)
 
-            time.sleep(60 * 29)
+            time.sleep(60 * 29)  # 29分钟检测一次, 保证一小时发送2次，最多不超过3次
 
     @staticmethod
     def send_notice(wday):
@@ -33,7 +34,11 @@ class SensoroDailyNotice(object):
             5: "五"
         }
         sensoro_logger.log(err="今天是周{}, 大家记得提交日报喔!".format(weekdays[wday]))
+        if wday == 5:  # 周五还需要提醒大家修改周报
+            sensoro_logger.log(
+                err="今天是周{}, 大家还需要去更新周报喔!"
+                    "周报链接: https://shimo.im/sheets/CTrdYQ8HRDtD3tVJ/CdyCZ".format(weekdays[wday]))
 
 
 sensoro_notice = SensoroDailyNotice()
-sensoro_notice.judge_time()
+sensoro_notice.run()
